097fc2bc798eb4a2b5bc656c896439b4387b07cc
Anders Tornøe <ato@kvuc.dk>
2016-01-06 14:56:45 +0100

    Demo version 1 til brugertest

--- mc_write_night.js
+++ mc_write_night.js
@@ -29,32 +29,90 @@ function loadData(url) {
 function init() {
 
     $(".btn_bland").click(bland_kategorier);
-    for (var i = 0; i < JsonObj.kategorier.length; i++) {;
-        $(".kategori_container").append("<div class='kat_obj_container col-xs-3'><div class='imgcontent'><img src='" + JsonObj.kategorier[i].imgcontent[Math.floor(Math.random() * JsonObj.kategorier[i].imgcontent.length)] + "' /></div><div class='content_header h4'>" + JsonObj.kategorier[i].tekst + "</div></div><div class='col-xs-1 glyph_container'> <span class='glyphicon glyphicon-plus-sign'></span></div>");
+
+    $(".btn_start_tid").click(startTimer);
+
+    for (var i = 0; i < JsonObj.kategorier.length; i++) {
+        var rand = Math.floor(Math.random() * JsonObj.kategorier[i].imgcontent.length);
+        $(".kategori_container").append("<div class='kat_obj_container col-xs-5 col-sm-3'><div class='imgcontent'><img src='" + JsonObj.kategorier[i].imgcontent[rand] + "' value='" + rand + "' /></div><div class='content_header h4'>" + JsonObj.kategorier[i].tekst + "</div></div><div class='col-xs-1 glyph_container'> <span class='glyphicon glyphicon-plus-sign'></span></div>");
     }
-    $(".glyph_container").eq($(".glyph_container").length - 1).hide();
+    //$(".glyph_container").eq($(".glyph_container").length - 1).hide();
+
+    $(".glyphicon").click(function() {
+        next_icon($(this));
+    });
 }
 
 
 function bland_kategorier() {
 
-
     $(".imgcontent").each(function(index) {
-        console.log(index + ": " + $(this).text());
         $(this).find("img").animate({
             opacity: "0",
         }, 200 + Math.random() * 300, function() {
-            console.log("anim complete");
-            $("img").eq(index).attr("src", JsonObj.kategorier[index].imgcontent[Math.floor(Math.random() * JsonObj.kategorier[index].imgcontent.length)])
+            var rand = Math.floor(Math.random() * JsonObj.kategorier[index].imgcontent.length);
+            $("img").eq(index).attr("src", JsonObj.kategorier[index].imgcontent[rand]).attr("value", rand)
             $("img").eq(index).animate({
                 opacity: "1"
             }, 200);
         });
-
-
         //$(this).attr("src", JsonObj.kategorier[index].imgcontent[Math.floor(Math.random() * JsonObj.kategorier[index].imgcontent.length)]);
-
     });
+}
+
+
+function startTimer() {
+
+    $(".btn_start_tid, .txt_tid, .p_tid").hide();
+    var minutes = $(".txt_tid").val();
+    var seconds = new Date().getTime() + (minutes * 60000);
+
+    console.log(seconds);
+    $('#clock').countdown(seconds, {
+            elapse: true
+        })
+        .on('update.countdown', function(event) {
+            var $this = $(this);
+            if (event.elapsed) {
+                $('div#clock').countdown('pause');
+                $(".btn_start_tid, .txt_tid, .p_tid").show();
+                $('div#clock').remove();
+                $(".tid_container").append("<div id='clock'></div>");
+                $(".btn_start_tid").html("Start nedtælling").click(startTimer);
+                UserMsgBox("body", "<h3>Tiden er udløbet</h3><p>Fik du skrevet et godt digt?</p>");
+
+            } else {
+                $this.html(event.strftime('Tid tilbage: <span>%H:%M:%S</span>'));
+            }
+        })
+}
+
+function stopTimer() {
+    $('div#clock').countdown('stop');
+    $(".btn_start_tid").html("Start nedtælling").off(); //.click(startTimer);
+}
+
+function next_icon(obj) {
+    var indeks = (obj.parent().index() - 1) / 2;
+    var new_img_value = parseInt($("img").eq(indeks).attr("value"));
+
+    new_img_value++;
+
+    $("img").eq(indeks).animate({
+        opacity: "0",
+    }, 200 + Math.random() * 300, function() {
+        if (new_img_value < JsonObj.kategorier[indeks].imgcontent.length) {
+            $("img").eq(indeks).attr("src", JsonObj.kategorier[indeks].imgcontent[new_img_value]).attr("value", new_img_value);
+        } else {
+            $("img").eq(indeks).attr("src", JsonObj.kategorier[indeks].imgcontent[0]).attr("value", 0);
+        }
+        $("img").eq(indeks).animate({
+            opacity: "1"
+        }, 200);
+    });
+
+
+
 
 
 
