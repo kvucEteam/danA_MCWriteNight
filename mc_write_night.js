c816ad628d26bf84715734aa62b3e83e46421202
Anders Tornøe <ato@kvuc.dk>
2016-02-08 13:04:33 +0100

    Click virker igen :D

--- mc_write_night.js
+++ mc_write_night.js
@@ -32,35 +32,10 @@ function init() {
 
     $(".btn_start_tid").click(startTimer);
 
-    if (JsonObj.userInterface.opg_type != "eventyr"){
-    /// Standard for objekt der ikke skal positionenes fast: 
-        for (var i = 0; i < JsonObj.kategorier.length; i++) {
-            var rand = Math.floor(Math.random() * JsonObj.kategorier[i].imgcontent.length);
-            $(".kategori_container").append("<div class='kat_obj_container col-xs-6 col-md-4'><div class='inner_container col-xs-10'> <div class='imgcontent' value='"+i+"'><img src='" + JsonObj.kategorier[i].imgcontent[rand] + "' value='" + rand + "' /></div><div class='content_header h3'>" + JsonObj.kategorier[i].tekst + "</div></div><div class='col-xs-1 glyph_container'> <span class='glyphicon glyphicon-plus-sign'></span></div></div>");
-    
-        }
-    } else if (JsonObj.userInterface.opg_type === "eventyr"){
-        for (var i = 0; i < JsonObj.kategorier.length; i++) {
-            var rand = Math.floor(Math.random() * JsonObj.kategorier[i].imgcontent.length);
-            $(".kategori_container").append("<div class='kat_obj_container col-xs-6 col-md-4 "+ JsonObj.kategorier[i].kat_type + "'><div class='inner_container col-xs-10'> <div class='imgcontent' value='"+i+"'><img src='" + JsonObj.kategorier[i].imgcontent[rand] + "' value='" + rand + "' /></div><div class='content_header h3'>" + JsonObj.kategorier[i].tekst + "</div></div><div class='col-xs-1 glyph_container'> <span class='glyphicon glyphicon-plus-sign'></span></div></div>");
-    
-        }
-
-        $(".odd").each(function(){
-            $(this).addClass("clone");            
-            $(this).clone().appendTo(".kategori_container");
-            $(this).addClass("hidden-xs hidden-sm").css("opacity", .3).removeClass("clone");
-        });
-
-        $(".clone").addClass("hidden-md hidden-lg");
-
-}
-
-    //// Eventyr exception: 
-
-
-
-
+    for (var i = 0; i < JsonObj.kategorier.length; i++) {
+        var rand = Math.floor(Math.random() * JsonObj.kategorier[i].imgcontent.length);
+        $(".kategori_container").append("<div class='kat_obj_container col-xs-6 col-md-4'><div class='inner_container col-xs-10'> <div class='imgcontent'><img src='" + JsonObj.kategorier[i].imgcontent[rand] + "' value='" + rand + "' /></div><div class='content_header h3'>" + JsonObj.kategorier[i].tekst + "</div></div><div class='col-xs-1 glyph_container'> <span class='glyphicon glyphicon-plus-sign'></span></div></div>");
+    }
     //$(".glyph_container").eq($(".glyph_container").length - 1).hide();
 
     $(".glyph_container, .imgcontent").click(function() {
@@ -71,14 +46,11 @@ function init() {
 
 function bland_kategorier() {
     $(".imgcontent").each(function(index) {
-        var indeks = parseInt($(this).attr("value"));
-        console.log("indeks: " + indeks);
-        $(this).find("img").delay(indeks * 150).animate({
+        $(this).find("img").delay(index * 150).animate({
             opacity: "0",
         }, 250, function() {
-            var rand = Math.floor(Math.random() * JsonObj.kategorier[indeks].imgcontent.length);
-            console.log("rand: " + rand);
-            $("img").eq(index).attr("src", JsonObj.kategorier[indeks].imgcontent[rand]).attr("value", rand)
+            var rand = Math.floor(Math.random() * JsonObj.kategorier[index].imgcontent.length);
+            $("img").eq(index).attr("src", JsonObj.kategorier[index].imgcontent[rand]).attr("value", rand)
             $("img").eq(index).animate({
                 opacity: "1"
             }, 300);
@@ -125,28 +97,22 @@ function stopTimer() {
 }
 
 function next_icon(obj) {
-    //obj.hide();
 
     var isimg = obj.hasClass("imgcontent");
 
     var parents = obj.closest("body");
 
     if (isimg === true) {
-        var indeks = obj.attr("value");
+        var indeks = obj.parent().parent().index();
     } else {
         var indeks = obj.parent().index();
     }
 
-    console.log("indeks:" + indeks);
-
     var new_img_value = parseInt($("img").eq(indeks).attr("value"));
-
-console.log("new_img_value:" + new_img_value);
 
     new_img_value++;
 
-
-    $(" .imgcontent#"+indeks).animate({
+    $("img").eq(indeks).animate({
         opacity: "0",
     }, 200 + Math.random() * 300, function() {
         if (new_img_value < JsonObj.kategorier[indeks].imgcontent.length) {
@@ -159,8 +125,10 @@ console.log("new_img_value:" + new_img_value);
         }, 200);
     });
 
-}
 
-$(document).ready(function() {
-    $('#instructionWrap').html(instruction(JsonObj.userInterface.instruktion));
-});
+
+
+
+
+
+}
